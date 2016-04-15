d7b743836fb807474a363c3123778e64f85e9ba3
unknown <zephyr.404.not.found@gmail.com>
2016-04-15 18:03:36 +0800

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,33 @@
+'use strict';
+$(function() {
+    // new WOW({ offset: 50 }).init();
+    // $('.hero-text').typed({
+    //     strings: ['Quick Prototype + ', 'Initial Traction + ', 'Launch Event', 'Quick Prototype + Initial Traction + Launch Event'],
+    //     typeSpeed: 50
+    // });
+    // var options = {
+    //     useEasing: true,
+    //     useGrouping: true,
+    //     separator: ','
+    // };
+    // var count = new CountUp('counter', 0, 1300000, 0, 4, options);
+    // var count1 = new CountUp('counter1', 0, 5400, 0, 4, options);
+
+
+    // function onScrollAnimations() {
+    //     $('.counter-one').waypoint(function() {
+    //         $('.counter-one').removeClass('hidden');
+    //         count.start();
+    //     }, {
+    //         offset: '75%'
+    //     });
+    //     $('.counter-two').waypoint(function() {
+    //         $('.counter-two').removeClass('hidden');
+    //         count1.start();
+    //     }, {
+    //         offset: '65%'
+    //     });
+    // }
+
+    // onScrollAnimations();
+});
